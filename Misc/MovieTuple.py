980ea51be335ca5ad93c9106aff584b19b6fe11a
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-13 20:11:08 -0500

    commit

---
+++ Misc/MovieTuple.py
@@ -0,0 +1,11 @@
+# movies = [("The Room", "Tommy Wiseau", "2003", "6,000,000")]
+
+# title = input("enter the movie title: ")
+# director = input("enter the movie director: ")
+# year = input("enter year the movie was made: ")
+# budget = input("enter the budget of the movie: ")
+
+# new_movie = (title, director, year, budget)
+# print(f"Title is {new_movie[0]} and it was released in {new_movie[2]}")
+# movies.append(new_movie)
+
