4c5cb4a14db63166157f24684ee1db1bbb8757d9
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-13 19:39:58 -0500

    commit

--- Misc/PyReview.py
+++ Misc/PyReview.py
@@ -1,4 +1,8 @@
 import math
 
-radius = 5
-print(math.pi * pow(radius,2))
+# radius = 5
+# print(math.pi * pow(radius,2))
+
+# name = input("Please enter your name: ")
+# age = input("Please enter your age: ")
+
