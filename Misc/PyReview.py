fe77331169a0d3a70f857ee56853dc1b32fddce8
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-01-15 16:25:19 -0500

    commit

--- Misc/PyReview.py
+++ Misc/PyReview.py
@@ -6,3 +6,24 @@ import math
 # name = input("Please enter your name: ")
 # age = input("Please enter your age: ")
 
+
+# AGE = 20
+# DAYS_IN_YEAR = 365
+# WEEKS_IN_YEAR = 52
+# MONTHS_IN_YEAR = 12
+
+# print(DAYS_IN_YEAR * 27)
+# print(WEEKS_IN_YEAR * 27)
+# print(MONTHS_IN_YEAR * 27)
+
+# # Formula of a circle = pi*r^2
+# radius = 5
+# radiusOfCircle = (math.pi * pow(radius, 2))
+# print(radiusOfCircle)
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print(f"Hello {name.capitalize()}, you are {age} years old")
+
+print(f"{math.pi:.4f}")
