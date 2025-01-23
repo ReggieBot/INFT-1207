c2df5ebd325b089147689b2d9c612f8a88580554
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-22 19:54:44 -0500

    week 3 advanced python

---
+++ Concepts/ReadWrite.py
@@ -0,0 +1,25 @@
+# import os
+
+
+# print(f'Drive: {os.getcwd()}')
+
+# # Opening and writing to a file (overrides what text is currently in the file)
+# example_file = open('example.txt', 'w')
+# example_file.write("Hello File!")
+# example_file.close()
+
+# # Opening and reading from a file
+# example_file = open('example.txt', 'r')
+# print(example_file.read())
+# example_file.close()
+
+# # Opening and appending to a file
+# example_file = open('example.txt', 'a')
+# example_file.write("Hello File 2!")
+# example_file.close()
+
+# with open("example.txt", "r") as example_file:
+#     print(example_file.read())
+
+# with open("example.txt", "a") as example_file:
+#     example_file.write("Hello File 3")
