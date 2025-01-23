6bf82bd692238b2fb8f0a6987a6ca8de7317d572
Reggie Brown <reggiestephenbrown@gmail.com>
2025-01-22 20:16:25 -0500

    using a dictionary

--- Concepts/CSV.py
+++ Concepts/CSV.py
@@ -1,3 +1,14 @@
 with open("iris.csv", "r") as iris_file:
-    iris_data = iris_file.read()
-    print(iris_data)
+    iris_data = iris_file.readlines()
+
+irises = []
+
+for row in iris_data[1:]:
+    sepal_length, sepal_width, petal_length, petal_width, species = row.strip().split(",")
+    irises_dict = {
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width,
+        "species": species
+    }
