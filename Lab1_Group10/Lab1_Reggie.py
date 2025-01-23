92a3201402926d045f35e6a74bcad759c6a7d782
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-01-23 16:41:15 -0500

    Started building generate_password() function

--- Lab1_Group10/Lab1_Reggie.py
+++ Lab1_Group10/Lab1_Reggie.py
@@ -4,3 +4,18 @@
 # Prof. Patel
 # Desc: This program wll generate a random password based on user defined requirements
 
+import random
+import string
+
+def generate_password(totalLength, numSpecial, numDigits, numLetters):
+    
+    # Basic validation for input
+    if totalLength < 8 or totalLength > 16:
+        return "Password length must be between 8 and 16 characters"
+    
+    # Checks remaining available characters after eac;h user input 
+    remainingLength = totalLength - numLetters
+
+    if remainingLength < 0:
+        return (f"Error: Letters must be between 0 - {totalLength}")
+    
