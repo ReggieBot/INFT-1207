b91f170e3c000de3c8b52566c259473825bd6e94
Reggie Brown <reggiestephenbrown@gmail.com>
2025-02-05 20:12:57 -0500

    added test_add_method_returns_correct_result

--- Week 5- Unit Test Part 1 Codes/UnitTestWeek5/test/test_addition.py
+++ Week 5- Unit Test Part 1 Codes/UnitTestWeek5/test/test_addition.py
@@ -3,3 +3,10 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app.addition import Addition
+
+class TestAddition(unittest.TestCase):
+    def test_add_method_returns_correct_result(self):
+        calc = Addition()
+        self.assertEqual(calc.add(2,2), 4)
