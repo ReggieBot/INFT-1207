b3ff9176b5dfd9ed9a1f5d3f59fad7fd5484eada
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-02-07 15:22:34 -0500

    Program blueprint created

--- Lab2_Group3/Lab2_Reggie_Wren.py
+++ Lab2_Group3/Lab2_Reggie_Wren.py
@@ -3,4 +3,28 @@
 # 07/02/25
 # Reggie Brown | reggie.brown@dcmail.ca
 # Wren Bailey | wren.bailey@dcmail.ca
+# This program allows a user to add, search, and view their current reading list
 
+import csv
+
+
+def add_book():
+
+# function to add book to the csv
+
+
+def list_books():
+
+# function to get all books from csv and display to user
+
+def search_books():
+
+# function to search for specific book in csv by name
+
+def main():
+
+# main function - this is where our menu will go
+
+
+if __name__ == '__main__':
+    main()
