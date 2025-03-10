5f1ca9f9b11dd5524b03b10ac828b3c9f89a0afe
Reggie Brown <reggiestephenbrown@gmail.com>
2025-03-10 19:48:21 -0400

    Testing with selenium

---
+++ IntroToSelenium/pytest_cache/selenium_test_class.py
@@ -0,0 +1,32 @@
+from time import sleep
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class DemoSeleniumTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.browser = webdriver.Firefox()
+        cls.browser.maximize_window()
+        # Open the following url in the browser
+        cls.browser.get("https://www.selenium.dev/selenium/web/web-form.html")
+        sleep(1)
+
+    def test_page_title(self):
+        self.assertEqual("Web form", self.browser.title)
+        sleep(1)
+
+    def test_fill_text_and_submit(self):
+        # find the Text input textbox
+        text_box = self.browser.find_element(By.NAME, "my-text")
+        # fill in the textbox using send_keys method
+        text_box.send_keys("Hello Selenium")
+        sleep(1)
+
+        # find the submit button using find_element
+        submit_button = self.browser.find_element(By.CSS_SELECTOR, "button")
+        # click the submit button
+        submit_button.click()
