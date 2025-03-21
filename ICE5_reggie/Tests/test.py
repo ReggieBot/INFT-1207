c92ac23f0d9712d5317095a4d118ac2e9e5d1faa
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-03-20 21:26:13 -0400

    Finished test case 2. Program in working state

--- ICE5_reggie/Tests/test.py
+++ ICE5_reggie/Tests/test.py
@@ -1,12 +1,15 @@
 # Reggie Brown
 # INFT 1207
 # Prof Patel
-# Desc: Created a new user on a test site with predefined credentials and validates successful auth using python/selenium
+# Desc: Created a new user on test site with predefined credentials and validates successful auth using python/selenium
 # References: https://www.browserstack.com/guide/isdisplayed-method-in-selenium
+# github: https://github.com/ReggieBot/INFT-1207/tree/main/ICE5_reggie/Tests
+
 from time import sleep
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 
 class MercuryToursTest(unittest.TestCase):
 
@@ -18,11 +21,11 @@ class MercuryToursTest(unittest.TestCase):
         cls.browser.get("https://demo.guru99.com/test/newtours/")
         sleep(2)
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        sleep(5)
-        cls.browser.quit()
-        # Tear down method for closing script after exec.
+    # @classmethod
+    # def tearDownClass(cls) -> None:
+    #     sleep(5)
+    #     cls.browser.quit()
+    #     # Tear down method for closing script after exec.
 
     def test1_Register_User(self):
         # First test case: Register user with username "Reggie" and password "durhamcollege2025"
@@ -62,7 +65,7 @@ class MercuryToursTest(unittest.TestCase):
 
         # Filling in registration details (user)
         # Why is the ID/name for Username = email? Who knows.
-        self.browser.find_element(By.ID, "email").send_keys("reggie")
+        self.browser.find_element(By.ID, "email").send_keys("Reggie")
         self.browser.find_element(By.NAME, "password").send_keys("durhamcollege2025")
         self.browser.find_element(By.NAME, "confirmPassword").send_keys("durhamcollege2025")
         sleep(1)
@@ -72,6 +75,36 @@ class MercuryToursTest(unittest.TestCase):
         sleep(1)
 
         # Assert that registration successful
-        registration_success = self.browser.find_element(By.XPATH, "//b[contains(text(),'Your user name is reggie.')]")
+        registration_success = self.browser.find_element(By.XPATH, "//b[contains(text(),'Your user name is Reggie.')]")
         self.assertTrue(registration_success.is_displayed())
         print("Registration Successful")
+        sleep(2)
+
+    def test2_Login_User(self):
+        # Second test case: Login with newly created user account and assert login successful
+        # Return to homepage
+        self.browser.get("https://demo.guru99.com/test/newtours/")
+        print("Successfully navigated to homepage")
+        sleep(2)
+
+        # Click on "sign on" - This site is riddled with typos
+        self.browser.find_element(By.LINK_TEXT, "SIGN-ON").click()
+        sleep(1)
+
+        # Login with new user account by locating elements and sending keys.
+        self.browser.find_element(By.NAME, "userName").send_keys("Reggie")
+        self.browser.find_element(By.NAME, "password").send_keys("durhamcollege2025")
+        sleep(1)
+
+        # Click on submit
+        self.browser.find_element(By.NAME, "submit").click()
+        sleep(2)
+
+        # Assert that login successful
+        newlogin_success = self.browser.find_element(By.XPATH, "//h3[contains(text(),'Login Successfully')]")
+        self.assertTrue(newlogin_success.is_displayed())
+        print("Successfully logged in with new user")
+
+
+if __name__ == '__main__':
+    unittest.main()
