adabf560d16334420e386b448cf6253ea8925d8d
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-04-09 12:05:14 -0400

    Finished test case 1

--- FinalProject_ReggieBrown/tests/test_edit_customer.py
+++ FinalProject_ReggieBrown/tests/test_edit_customer.py
@@ -8,6 +8,7 @@
 
 # REFERENCES: 
 # https://www.selenium.dev/documentation/webdriver/elements/interactions/      .clear()
+# https://stackoverflow.com/questions/38022658/selenium-python-handling-no-such-element-exception noSuchElementException
 
 # This test file assumes that a customer accont has already been created.
 # Account created by me - Customer ID = 43593
@@ -16,6 +17,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from selenium.common.exceptions import NoSuchElementException
 
 class TestEditCustomer(unittest.TestCase):
 
@@ -28,7 +30,7 @@ class TestEditCustomer(unittest.TestCase):
         cls.browser.maximize_window()
 
         # nav to site
-        cls.browser.get("http://demo.guru99.com/v4/")
+        cls.browser.get("http://demo.guru99.com/V4/")
         sleep(1.5)
 
         # log in (manager credentials)
@@ -44,18 +46,18 @@ class TestEditCustomer(unittest.TestCase):
             sleep(2)
             cls.browser.quit()
 
-        # various methods for use in test cases
+    # various methods for use in test cases
 
-        # cick on 'edit customer' link
-        def go_edit_customer(self):
-            self.browser.find_element(By.LINK_TEXT, "Edit Customer").click()
-            sleep(1)
+    # cick on 'edit customer' link
+    def go_edit_customer(self):
+        self.browser.find_element(By.LINK_TEXT, "Edit Customer").click()
+        sleep(1)
 
-        # enter customer id + submit
-        def submit_cust_id(self, customer_id):
-            self.browser.find_element(By.NAME, "cusid").send_keys(customer_id)
-            self.browser.find_element(By.NAME, "AccSubmit").click()
-            sleep(1)
+    # Enter customer id + submit
+    def submit_cust_id(self, customer_id):
+        self.browser.find_element(By.NAME, "cusid").send_keys(customer_id)
+        self.browser.find_element(By.NAME, "AccSubmit").click()
+        sleep(1)
 
 
 
@@ -65,10 +67,64 @@ class TestEditCustomer(unittest.TestCase):
         self.go_edit_customer()
         self.submit_cust_id("43593")
         
-        # Update editable fields (name, gender, DOB - not editable)
-        # 
-        self.browser.find_element(By.NAME)
+        # update editable fields (name, gender, DOB - not editable)
+        
+        # address
+        self.browser.find_element(By.NAME, "addr").clear()
+        self.browser.find_element(By.NAME, "addr").send_keys("36 Hockley Avenue")
+ 
+        # city
+        self.browser.find_element(By.NAME, "city").clear()
+        self.browser.find_element(By.NAME, "city").send_keys("Oshawa")
+
+        # state (province)
+        self.browser.find_element(By.NAME, "state").clear()
+        self.browser.find_element(By.NAME, "state").send_keys("Alberta")
+
+        # pin
+        self.browser.find_element(By.NAME, "pinno").clear()
+        self.browser.find_element(By.NAME, "pinno").send_keys("696969")
+
+        # phone #
+        self.browser.find_element(By.NAME, "telephoneno").clear()
+        self.browser.find_element(By.NAME, "telephoneno").send_keys("911")
+
+        # email
+        self.browser.find_element(By.NAME, "emailid").clear()
+        self.browser.find_element(By.NAME, "emailid").send_keys("testemail@gmail.com")
+        sleep(.5)
+
+        # sumbit changes
+        self.browser.find_element(By.NAME, "sub").click()
+        sleep(1)
 
 
+        # check for success message
+        try: 
+            success_msg = self.browser.find_element(By.XPATH, "//p[contains(text(),'Edit Successfull')]")
+            self.assertTrue(success_msg.is_displayed())
+            print("Test1 passed: Valid edit successful.")
+
+        except NoSuchElementException:
+            print("No success message found")
+
+            # check if data was still updated
+            self.browser.get("http://demo.guru99.com/V4/")
+            sleep(.5)
+            self.browser.find_element(By.NAME, "uid").send_keys("mngr619261")
+            self.browser.find_element(By.NAME, "password").send_keys("anugagy")
+            self.browser.find_element(By.NAME, "btnLogin").click()
+            self.go_edit_customer()
+            self.submit_cust_id("43593")
+            sleep(1)
+
+            current_address = self.browser.find_element(By.NAME, "addr").get_attribute("value")
+
+            if current_address == "36 Hockley Avenue":
+                print("Data updated successfully. Success message not displayed. Test 1 failed.")
+            else:
+                print("No success message AND data not updated. Test 1 failed.")
+
+        
 if __name__ == '__main__':
     unittest.main()
