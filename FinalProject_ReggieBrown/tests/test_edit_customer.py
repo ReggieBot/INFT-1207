09814f9ff28c533d8ae9df747f683c4965886277
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-04-09 10:08:20 -0400

    Updated test_edit_customer.py to include initial comments/description

---
+++ FinalProject_ReggieBrown/tests/test_edit_customer.py
@@ -0,0 +1,7 @@
+# Reggie Brown
+# reggie.brown@dcmail.ca
+# INFT 1207 - Final Project 
+# Prof. Patel
+# Desciption: This file contains 20 test cases for the "edit customer" 
+# functionality of the guru99 bank application. Test cases were written with pytest & selenium
+# Github: https://github.com/ReggieBot/INFT-1207/tree/main/FinalProject_ReggieBrown
