b600ef084228579737f97de608069a2d2aac3ca6
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2025-04-11 19:01:21 -0400

    Various utility methods for use later in the program

--- FinalProject_ReggieBrown/tests/test_delete_customer.py
+++ FinalProject_ReggieBrown/tests/test_delete_customer.py
@@ -24,5 +24,53 @@ from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 class TestDeleteCustomer(unittest.TestCase):
-    # 8 Test cases 
+    # 8 test cases for the delete customer functionality
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        # set up webdriver (firefox)
+        cls.browser = webdriver.Firefox()
+        cls.browser.maximize_window()
+
+        # nav to site
+        cls.browser.get("http://demo.guru99.com/V4/")
+        sleep(1.5)
+
+        # log in with manager credentials
+        cls.browser.find_element(By.NAME, "uid").send_keys("mngr619261")
+        cls.browser.find_element(By.NAME, "password").send_keys("anugagy")
+        cls.browser.find_element(By.NAME, "btnLogin").click()
+        sleep(1.5)
+
+        print("Manager login successful")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        # close browser
+        sleep(2)
+        cls.browser.quit()
+        
+    # various methods for use in test cases
+    def go_delete_customer(self):
+        # nav to delete customer page
+        self.browser.find_element(By.LINK_TEXT, "Delete Customer").click()
+        sleep(1)
+
+    def enter_customer_id(self, customer_id):
+        # enter customer id and submits
+        customer_id_field = self.browser.find_element(By.NAME, "cusid")
+        customer_id_field.clear()
+        customer_id_field.send_keys(customer_id)
+        sleep(.5)
+        # submit
+        self.browser.find_element(By.NAME, "AccSubmit").click()
+        sleep(1)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
